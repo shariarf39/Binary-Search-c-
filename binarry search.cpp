de71f214cf84468a4502ef3efa60b0d1f21f3b9b
MD Fahim Shariar <110589927+shariarf39@users.noreply.github.com>
2023-06-13 20:26:18 +0600

    Update binarry search.cpp

--- binarry search.cpp
+++ binarry search.cpp
@@ -1,37 +1,49 @@
-
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int binarySearch(int arr[], int l, int r, int x)
-{
-	while (l <= r) {
-		int m = l + (r - l) / 2;
+int binary(int arr[], int len, int input){
+
+int low=0, high = len;
+
+while(low<=high){
+
+    int half = (low+high)/2;
+
+    if(arr[half]==input){
+        return half;
+    }
+
+    if(input>arr[half]){
+            low = half +1;
 
 
-		if (arr[m] == x)
-			return m;
+    }
+        else{
+            high = half -1;
+
+        }
+
+        }
+
+        return -1;
 
 
-		if (arr[m] < x)
-			l = m + 1;
 
 
-		else
-			r = m - 1;
-	}
-
-	return -1;
 }
+int main(){
 
-// Driver code
-int main(void)
-{
-	int arr[] = { 2, 3, 4, 10, 40 };
-	int x = 10;
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int result = binarySearch(arr, 0, n - 1, x);
-	(result == -1)
-		? cout << "Element is not present in array"
-		: cout << "Element is present at index " << result;
-	return 0;
+int arr[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+int len = end(arr)-begin(arr);
+int input;
+cin>>input;
+//cout<<len;
+
+int index = binary(arr, len, input);
+
+if(index==-1)
+cout<<"Not Found"<<endl;
+else
+    cout<< "Found  "<<index;
+
 }
